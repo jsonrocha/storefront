f7f5080e06ac324cf5127cf0607680b38c47989f
Rocha <jrocha@proforma.com>
2018-08-25 23:24:33 -0400

    CSS changes

--- storefront/src/App.js
+++ storefront/src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
       <Router> 
       <div className="App">
         <header className="App-header">
-        <img src="https://i.scdn.co/image/49f64d0079acea59cd65a49688f7cfba673714d1"></img>
+        <img className="pic" src="https://i.scdn.co/image/49f64d0079acea59cd65a49688f7cfba673714d1"></img>
+        <h3 className="banner">Specializing in PlayStation 2 Games From the Early 2000's</h3>
         </header>
         <Switch>
           <Route path="/" exact component={CategoryList}/>
